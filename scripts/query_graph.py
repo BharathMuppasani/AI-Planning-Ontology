038e03df01ad5d8fc555a40d7df089fdd0e79c9f
Bharath Chandra <muppasanibharath.18@gmail.com>
2022-07-28 16:05:09 -0400

    website update

---
+++ scripts/query_graph.py
@@ -0,0 +1,52 @@
+from unittest import result
+from numpy import var
+from rdflib import *
+from rdflib import Namespace
+from rdflib.term import URIRef
+from rdflib.plugins import sparql
+from rdflib.namespace import RDF, RDFS, OWL
+from regex import P
+from uritemplate import variables
+
+def graph_query(sql_query, g):
+    
+    list = sql_query.split('\n')
+
+    for line in list:
+        if 'PREFIX' in line or 'PREFIX'.lower() in line:
+            prefix_list = [ x for x in line.split(" ") if x]
+            dict = {}
+            dict[prefix_list[1].replace(':','')] = Namespace(prefix_list[-1].replace('<','').replace('>',''))
+            locals().update(dict)
+    
+    variables = []
+    for line in list:
+        if 'SELECT' in line or 'SELECT'.lower() in line:
+            temp_list = line.split(" ")
+            for item in temp_list:
+                if '?' in item:
+                    variables.append(item.replace('?','').rstrip())
+    
+    new_query_list = []
+    for line in list:
+        if 'PREFIX' in line or 'PREFIX'.lower() in line:
+            continue
+        new_query_list.append(line)
+    
+    new_query = '\n'.join(new_query_list)
+    results = g.query(new_query)
+    
+    output_dict = {}
+    for output in results:
+        for idx in range(len(output)):
+            if variables[idx] not in output_dict:
+                output_dict[variables[idx]] = [output[idx].split("#")[-1]]
+            else:
+                output_dict[variables[idx]].append(output[idx].split("#")[-1])
+    
+    all_headers = output_dict.keys()
+    all_rows = zip(*output_dict.values())
+    
+    return all_headers,all_rows
+            
+
